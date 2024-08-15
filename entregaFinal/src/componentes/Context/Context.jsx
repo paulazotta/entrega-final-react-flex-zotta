eb2a751b0378a1c9793a8d5d58cbeedda6e12655
paulazotta <paezotta@gmail.com>
2024-08-14 21:17:22 -0300

    firestore2

--- entregaFinal/src/componentes/Context/Context.jsx
+++ entregaFinal/src/componentes/Context/Context.jsx
@@ -1,5 +1,22 @@
 import React, { createContext, useContext, useState } from 'react'
 import arrayProductos from '../Productos/arrayProductos';
+import {initializeApp} from "firebase/app";
+import {collection, getDocs, getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjOUbLlh3kF6D38EnFhK_M7onjL3LKH8E",
+  authDomain: "react-flex-paulazotta.firebaseapp.com",
+  projectId: "react-flex-paulazotta",
+  storageBucket: "react-flex-paulazotta.appspot.com",
+  messagingSenderId: "825040831188",
+  appId: "1:825040831188:web:56faee867500ad6ad7f141"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore (app)
+
+const productsCollection = collection (db, "productos");
 
 // 1. Creo las funciones para usar el contexto
 // Creo el contexto 
@@ -16,6 +33,13 @@ export const ContextProvider = (props) => {
     const [carrito, setCarrito] = useState ([]);
 
     function cargarData () {
+
+    getDocs(productsCollection).then(snapshot => {
+      console.log(snapshot)
+    })
+
+
+
         arrayProductos()
         .then(response => {
             setProductos(response);
